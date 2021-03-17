8b982ef4d9361e8a4c3a6fec208ef99adb5261d1
bchukwuemeka <bchukwuemeka@boxdevlab.com>
2021-03-17 06:54:12 +0300

    deploymenting to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-	devServer: {
-    inline:true,
-    port: 8008
-  },
+	// devServer: {
+  //   inline:true,
+  //   port: 8008
+  // },
 	// plugins: [new HtmlWebpackPlugin({
 	// 		template: './index.html'
 	// 	})],
