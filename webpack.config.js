ca0ff9eb60095da4623f6996d89be84c17afb5b4
bchukwuemeka <bchukwuemeka@boxdevlab.com>
2021-03-17 11:30:15 +0300

    deploymenting to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 	devServer: {
    disableHostCheck: true,
 	
-	 port: process.env.PORT || 3000,
+	 port: process.env.PORT || 80,
   },
 	// plugins: [new HtmlWebpackPlugin({
 	// 		template: './index.html'
