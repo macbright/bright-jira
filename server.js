99b0737bbeea190646cf55249352c3ad5db7b60b
bchukwuemeka <bchukwuemeka@boxdevlab.com>
2021-03-17 10:39:24 +0300

    deploymenting to heroku

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express');
 var app = express();
 
 
-app.set('port', process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
